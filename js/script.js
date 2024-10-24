43037f32f1fbd967fbeb78f8510aba7be1b5815f
abderrazzak imily <imily2024@gmail.com>
2024-10-24 13:05:40 +0100

    testing js posible

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let but = document.getElementById("arrowbut1");
+let but1 = document.getElementById("arrowbut2");
+let maige = document.getElementById("main-image");
+let cmaige = document.getElementById("image23");
+
+const image = ['/assets/img detait/Image (1).png' , '/assets/img detait/Image (2).png']
+
+
+
+but.addEventListener('click' , envet => {
+    maige = cmaige;
+});
+
+but1.addEventListener('click' , envet => {
+    alert("hellow i am imily");
+});
